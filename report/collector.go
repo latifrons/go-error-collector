f8269c9185aecc170d97f7c8575652aaa76b9696
latfirons <latifrons88@gmail.com>
2023-12-13 15:10:45 +0800

    add report error

--- report/collector.go
+++ report/collector.go
@@ -16,6 +16,25 @@ func Report(component string, severity int, message string, stacktrace string) {
 		Severity:   severity,
 	})
 }
+func ReportError(component string, severity int, message string, err error) {
+	if err != nil {
+		Reporter.Report(goerrorcollector.ErrorMessage{
+			Component:  component,
+			Message:    message + " " + err.Error(),
+			Stacktrace: "",
+			Time:       time.Now().UnixMilli(),
+			Severity:   severity,
+		})
+	} else {
+		Reporter.Report(goerrorcollector.ErrorMessage{
+			Component:  component,
+			Message:    message,
+			Stacktrace: "",
+			Time:       time.Now().UnixMilli(),
+			Severity:   severity,
+		})
+	}
+}
 
 func Start(url string, exchange string, topic string) {
 	Reporter.Start(goerrorcollector.WithReceiverRabbitMQ(goerrorcollector.ReceiverMqOption{
